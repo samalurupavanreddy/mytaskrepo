f29f4e2fc606bc8489ca0fb38e778be4e0f7e615
samalurupavan <samalurupavan1998@gmail.com>
2021-12-07 18:07:53 +0530

    myfiles for project

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+#provider "aws" {
+ # region = var.aws_region
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key#
+}
+
 provider "aws" {
-  region = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region     =  "us-east-1"
+  shared_credentials_file = "c:/Users/lenovo/.aws/credentials"
+  profile                 = "default"
 }
